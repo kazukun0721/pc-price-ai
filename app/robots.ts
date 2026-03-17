3917d9d205ce62597bb7b2e6577252aeec4c7e3c
kazu okuno <kazukun0721@gmail.com>
2026-03-18 01:24:40 +0900

    seo fix

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://pc-price-ai-cdf6.vercel.app/sitemap.xml",
+  };
+}
